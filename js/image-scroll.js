31303450a9f8a2d821cd4ad5e15d1e512c77bdd0
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2020-01-25 01:53:01 +0900

    fix

--- js/image-scroll.js
+++ js/image-scroll.js
@@ -50,3 +50,111 @@ $(function(){
         });
     });
 });
+
+// $('.slider').slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow:3,
+//     slidesToScroll: 1,
+// });
+
+// $(function(){
+//     const $items = $('.slider');
+//     const $page = $('.slider.slide__item');
+//     const shiftWidth = $page.outerWidth(true);
+//     const shiftTime = 800;
+//     // ----次へ----
+//     const Next = () => {
+//         $items.stop().animate({
+//         left: -shiftWidth
+//         },function(){
+//         $items.append($items.find('.slider').eq(0));
+//         $items.css({left:0});
+//         $items.find('.slider').eq(0).removeClass('center');
+//         $items.find('.slider').eq(1).addClass('center');
+//         });
+//     }
+//     // ----前へ----
+//     const Prev = () => {
+//         $items.animate({
+//         left: 0
+//         },function(){
+//         $items.prepend($items.find('.slider').eq(length-1));
+//         $items.css({left: -shiftWidth});
+//         $items.find('.slider').eq(3).removeClass('center');
+//         $items.find('.slider').eq(2).addClass('center');
+//         });
+//     }
+//     // ----次へボタン----
+//     $('#next').click(function(){
+//         Next();
+//     });
+//     // ----前へボタン----
+//     $('#prev').click(function(){
+//         Prev();
+//     });
+//     // -----自動送り----
+//     setInterval(Next,6000);
+// });
+
+
+
+$(function(){
+    const $items = $('#content-items');
+    const $page = $('#content-items li');
+    const shiftWidth = $page.outerWidth(true);
+    const shiftTime = 800;
+  
+  
+    // ----谺｡縺ｸ----
+    const Next = () => {
+      $items.stop().animate({
+        left: -shiftWidth
+      },function(){
+        $items.append($items.find('li').eq(0));
+        $items.css({left:0});
+        $items.find('li').eq(0).removeClass('center');
+        $items.find('li').eq(1).addClass('center');
+      });
+  
+    }
+  
+    // ----蜑阪∈----
+    const Prev = () => {
+      $items.animate({
+        left: 0
+      },function(){
+        $items.prepend($items.find('li').eq(length-1));
+        $items.css({left: -shiftWidth});
+        $items.find('li').eq(3).removeClass('center');
+        $items.find('li').eq(2).addClass('center');
+      });
+  
+    }
+  
+    // ----谺｡縺ｸ繝懊ち繝ｳ----
+    $('#next').click(function(){
+      Next();
+    });
+  
+    // ----蜑阪∈繝懊ち繝ｳ----
+    $('#prev').click(function(){
+      Prev();
+    });
+  
+    // -----閾ｪ蜍暮√ｊ----
+    setInterval(Next,6000);
+  });
+  
+  
+  $(function(){
+    $('.open').click(function(){
+      $(this).fadeOut();
+      $('header').slideDown();
+    })
+  
+    $('.close').click(function(){
+      $('.open').fadeIn();
+      $('header').slideUp();
+    });
+  });
