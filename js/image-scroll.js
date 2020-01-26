6ec16bb9e6d5b06b04e25c865c3b3e639ff9b258
TsukadaTomoya <xftpt.nonoka@gmail.com>
2020-01-26 15:59:30 +0900

    modalできぬ

--- js/image-scroll.js
+++ js/image-scroll.js
@@ -122,17 +122,7 @@ $(function(){
 
   $(function(){
     $('.navbar-toggler').on('click', function() {
-        if('.wrap.active'){
-            $('.menu').remove();
-        }
-
         $(this).toggleClass('active');
-        $('.wrap').toggleClass('active');
-
-        if('.wrap.active'){
-            $('.wrap.active').prepend('<div class="menu">AfterBegin</div>');
-        }
-
         return false;
         });
   });
