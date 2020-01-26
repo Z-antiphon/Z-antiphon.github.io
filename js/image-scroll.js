23be7c2aa763c65b1df19fab2f9475ffd14b1956
yutoyama <tymy1230@gmail.com>
2020-01-26 19:13:06 +0900

    test scrool

--- js/image-scroll.js
+++ js/image-scroll.js
@@ -165,17 +165,17 @@ $(function(){
   });
 
   // ページtopスクロールイベント
-$('a[href^=#]').click(function(){
+$(function scrooltop(){
   //デフォルトのイベントをキャンセル
-  // event.preventDefault();
+  event.preventDefault();
 
   // 移動先となる要素を取得します。
-  var target = $(this.hash);
+  var target = $('#main-visual');
   if (!target.length) return;
 
   // 移動先の位置を取得します
   var targetY = target.offset().top;
 
   // animateで移動します
-  $('body').animate({scrollTop: targetY}, 500, 'swing');
+  $('body').animate({scrollTop: targetY}, 1000, 'swing');
 });
