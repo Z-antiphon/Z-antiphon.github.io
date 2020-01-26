2c60c0413cdaae4d17d855a391ab68c569437429
yutoyama <tymy1230@gmail.com>
2020-01-26 18:58:35 +0900

    スクロールイベント

--- js/image-scroll.js
+++ js/image-scroll.js
@@ -163,3 +163,19 @@ $(function(){
       });
     }
   });
+
+  // ページtopスクロールイベント
+$('a[href^=#]').click(function(){
+  //デフォルトのイベントをキャンセル
+  // event.preventDefault();
+
+  // 移動先となる要素を取得します。
+  var target = $(this.hash);
+  if (!target.length) return;
+
+  // 移動先の位置を取得します
+  var targetY = target.offset().top;
+
+  // animateで移動します
+  $('body').animate({scrollTop: targetY}, 500, 'swing');
+});
